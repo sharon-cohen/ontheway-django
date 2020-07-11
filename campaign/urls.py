4be6d9a7ced1fffdd877a97c600c393fb3862364
sharon-choen <sharon49510@gmail.com>
2020-07-11 11:05:33 +0300

    url fix

--- campaign/urls.py
+++ campaign/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home.as_view(),),
     path('file', views.fiels,name='files'),
-    path('form', views.form,name='files'),
+    path('form', views.form,name='form'),
 ]
